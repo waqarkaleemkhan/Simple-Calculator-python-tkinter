eb29ea3634caf645fe89215cfc193d5afa44e7dc
waqarkaleemkhan <waqar.musa777@gmail.com>
2021-02-17 17:07:18 +0500

    addition functionality is added

--- simplecal.py
+++ simplecal.py
@@ -6,7 +6,24 @@ root.title("Simple Calculator")
 inputField=Entry(root,width=35,borderwidth=2)
 inputField.grid(row=0,column=0,columnspan=3,padx=10,pady=10)
 def button_click(number):
-	inputField.insert(0,number)
+	current = inputField.get() #use this to enter number in proper way like calculator
+	inputField.delete(0,END) #this will delete the previous number which is in the number parameter if we don;t do this it put all the previous number in the screen
+	inputField.insert(0,str(current)+str(number))
+
+
+def button_clear():
+	inputField.delete(0,END)
+
+def button_add():
+	first_number=inputField.get()
+	global f_num # we use global variable becuase we are going to accsess this from out side of the function with equal and addition function
+	f_num=int(first_number)
+	inputField.delete(0,END)
+def button_equal():
+	second_number=inputField.get()
+	inputField.delete(0,END)
+	inputField.insert(0,f_num+int(second_number))
+
 button_1=Button(root,text="1",padx=40,pady=20,command=lambda:button_click(1))
 button_2=Button(root,text="2",padx=40,pady=20,command=lambda:button_click(2))
 button_3=Button(root,text="3",padx=40,pady=20,command=lambda:button_click(3))
@@ -17,9 +34,9 @@ button_7=Button(root,text="7",padx=40,pady=20,command=lambda:button_click(7))
 button_8=Button(root,text="8",padx=40,pady=20,command=lambda:button_click(8))
 button_9=Button(root,text="9",padx=40,pady=20,command=lambda:button_click(9))
 button_0=Button(root,text="0",padx=40,pady=20,command=lambda:button_click(0))
-button_addition=Button(root,text="+",padx=39,pady=20,command=lambda:button_click())
-button_clear=Button(root,text="Clear",padx=79,pady=20,command=lambda:button_click())
-button_equal=Button(root,text="=",padx=91,pady=20,command=lambda:button_click())
+button_addition=Button(root,text="+",padx=39,pady=20,command=button_add)
+button_clear=Button(root,text="Clear",padx=79,pady=20,command=button_clear)
+button_equal=Button(root,text="=",padx=91,pady=20,command=button_equal)
 
 button_1.grid(row=3,column=0)
 button_2.grid(row=3,column=1)
